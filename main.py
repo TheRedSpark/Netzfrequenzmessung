dd26a833d0a69fd1e085458dad495593d2896287
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2023-01-13 18:01:32 +0100

    Grobes Grundgerüst Bot ist live

--- main.py
+++ main.py
@@ -1,15 +1,54 @@
 import requests
 import math as Math
+from package import variables as v
+import logging
+
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup  # 20.0a1
+from telegram.ext import Application, CommandHandler, ContextTypes, CallbackQueryHandler, MessageHandler, \
+    filters, ConversationHandler  # 20.0a1
+
+on_server = False
+version = "1.0"
+is_live = True
+
+if on_server:
+    pass
+else:
+    logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 
 
-##Math.round(Math.random()*100000)*31
+def netzfrequenz_pull():
+    ##Math.round(Math.random()*100000)*31
+
+    x = requests.get('https://netzfrequenzmessung.de:9081/frequenz02a.xml?c=916856')
+    y = x.text.splitlines()
+    # print(y)
+    netzfrequenz = y[1].replace("<f2>", "").replace("</f2>", "")
+    zeit = y[3].replace("<z> ", "").replace("</z>", "")
+
+    print(zeit)
+    print(netzfrequenz)
 
 
-x = requests.get('https://netzfrequenzmessung.de:9081/frequenz02a.xml?c=916856')
-y = x.text.splitlines()
-#print(y)
-netzfrequenz = y[1].replace("<f2>", "").replace("</f2>", "")
-zeit = y[3].replace("<z> ", "").replace("</z>", "")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await context.bot.send_message(update.effective_user.id,
+                                   text=f"Der Netzfrequenzmessung-Bot sagt herzlich hallo ;-)\n"
+                                        f"Netzfrequenzmessung-Bot {version}")
+    await context.bot.send_message(update.effective_user.id,
+                                   text=f'Benutze /help um Hilfe mit den Befehlen und der Funktionsweise des Bots zu '
+                                        'erhalten. \n')
 
-print(zeit)
-print(netzfrequenz)
+
+def main() -> None:
+    # Creating a telegram bot.
+    application = Application.builder().token(v.telegram_netzfrequenz_api(is_live)).build()
+
+    # Adding the handlers for the commands.
+    application.add_handler(CommandHandler(["start", "help"], start))
+
+    application.run_polling(1)
+
+
+if __name__ == '__main__':
+    main()
