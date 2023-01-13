4b48d9f6b116a5bcf5253e13ed42305efc818f66
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2023-01-13 19:16:20 +0100

    Userlogging zu Debug zwecken hinzugefügt

--- main.py
+++ main.py
@@ -18,6 +18,58 @@ else:
     logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 
 
+def user_create(user_id, username):
+    mydb = mysql.connector.connect(
+        host=v.host(ort),
+        user=v.user(ort),
+        passwd=v.passwd(ort),
+        database=v.database(),
+        auth_plugin='mysql_native_password')
+
+    my_cursor = mydb.cursor()
+    my_cursor.execute(f"SELECT User_Id,Username FROM `Netzfrequenmessung`.`Users` WHERE User_Id = ({user_id}) ")
+    result = my_cursor.fetchone()
+    if result is None:
+        sql_maske = "INSERT INTO `Netzfrequenmessung`.`Users` (`User_Id`,`Username`) VALUES (%s, %s); "
+        data_n = (user_id, username)
+        my_cursor.execute(sql_maske, data_n)
+        mydb.commit()
+    else:
+        pass
+    my_cursor.close()
+
+
+def userlogging(user_id, username, message_chat_id, message_txt, message_id, first_name, last_name, land_code):
+    mydb = mysql.connector.connect(
+        host=v.host(ort),
+        user=v.user(ort),
+        passwd=v.passwd(ort),
+        database=v.database(),
+        auth_plugin='mysql_native_password')
+
+    my_cursor = mydb.cursor()
+    time_sql = time.strftime("%Y-%m-%d %H:%M:%S")
+    if is_live:
+
+        sql_maske = "INSERT INTO `Netzfrequenmessung`.`Messages` (`Time`,`User_Id`,`Username`,`Chat_Id`,`Message_Text`," \
+                    "`Message_Id`,`First_Name`,`Last_Name`,`Land_Code`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s); "
+        data_n = (
+            time_sql, user_id, username, message_chat_id, message_txt, message_id, first_name, last_name, land_code)
+        my_cursor.execute(sql_maske, data_n)
+        mydb.commit()
+        my_cursor.close()
+
+    else:
+
+        sql_maske = "INSERT INTO `Netzfrequenmessung`.`Messagesb` (`Time`,`User_Id`,`Username`,`Chat_Id`,`Message_Text`," \
+                    "`Message_Id`,`First_Name`,`Last_Name`,`Land_Code`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s); "
+        data_n = (
+            time_sql, user_id, username, message_chat_id, message_txt, message_id, first_name, last_name, land_code)
+        my_cursor.execute(sql_maske, data_n)
+        mydb.commit()
+        my_cursor.close()
+
+
 def netzfrequenz_pull():
     ##Math.round(Math.random()*100000)*31
 
@@ -32,6 +84,10 @@ def netzfrequenz_pull():
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    userlogging(update.effective_user.id, update.effective_user.username, update.effective_message.chat_id,
+                update.effective_message.text_markdown, update.effective_message.id, update.effective_user.first_name,
+                update.effective_user.last_name, update.effective_user.language_code)
+    user_create(update.effective_user.id, update.effective_user.username)
     await context.bot.send_message(update.effective_user.id,
                                    text=f"Der Netzfrequenzmessung-Bot sagt herzlich hallo ;-)\n"
                                         f"Netzfrequenzmessung-Bot {version}\n"
@@ -42,17 +98,28 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 async def netzfrequenz(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    userlogging(update.effective_user.id, update.effective_user.username, update.effective_message.chat_id,
+                update.effective_message.text_markdown, update.effective_message.id, update.effective_user.first_name,
+                update.effective_user.last_name, update.effective_user.language_code)
     frequenz = netzfrequenz_pull()
     await context.bot.send_message(update.effective_user.id,
                                    text=f"Die aktuelle Netzfrequenz beträgt: {frequenz}Hz")
 
+
 async def mitmachen(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    userlogging(update.effective_user.id, update.effective_user.username, update.effective_message.chat_id,
+                update.effective_message.text_markdown, update.effective_message.id, update.effective_user.first_name,
+                update.effective_user.last_name, update.effective_user.language_code)
     await context.bot.send_message(update.effective_user.id,
                                    text=f"https://github.com/TheRedSpark/Netzfrequenzmessung")
     await context.bot.send_message(update.effective_user.id,
                                    text=f"Schau dir gerne das Repo an vielleicht möchtest du dich ja beteiligen.")
 
+
 async def info(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    userlogging(update.effective_user.id, update.effective_user.username, update.effective_message.chat_id,
+                update.effective_message.text_markdown, update.effective_message.id, update.effective_user.first_name,
+                update.effective_user.last_name, update.effective_user.language_code)
     await context.bot.send_message(update.effective_user.id,
                                    text=f"Um einen guten Einstieg in das Thema zu erhalten empfehle ich dir diesen CC3 Talk\n")
     await context.bot.send_message(update.effective_user.id,
