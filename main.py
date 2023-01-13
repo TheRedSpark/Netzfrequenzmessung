455a74de33a0f5636da6afefbece51848e4c996d
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2023-01-13 17:48:05 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+import math as Math
+
+
+##Math.round(Math.random()*100000)*31
+
+
+x = requests.get('https://netzfrequenzmessung.de:9081/frequenz02a.xml?c=916856')
+y = x.text.splitlines()
+#print(y)
+netzfrequenz = y[1].replace("<f2>", "").replace("</f2>", "")
+zeit = y[3].replace("<z> ", "").replace("</z>", "")
+
+print(zeit)
+print(netzfrequenz)
